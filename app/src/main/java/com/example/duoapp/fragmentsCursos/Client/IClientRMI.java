2f3ed2d14eabfd9993375ae1197213bd9a89b710
samirbenbouker <samirbenbouker@gmail.com>
2020-12-09 11:53:42 +0100

    client fet

---
+++ app/src/main/java/com/example/duoapp/fragmentsCursos/Client/IClientRMI.java
@@ -0,0 +1,8 @@
+package com.example.duoapp.fragmentsCursos.Client;
+
+import java.util.ArrayList;
+
+public interface IClientRMI {
+
+    public ArrayList<String> getTotsIdiomes();
+}
--- app/src/main/java/com/example/duoapp/fragmentsCursos/Cursos.java
+++ app/src/main/java/com/example/duoapp/fragmentsCursos/Cursos.java
@@ -1,5 +1,6 @@
 package com.example.duoapp.fragmentsCursos;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,28 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 
+import com.example.duoapp.MainActivity;
 import com.example.duoapp.R;
+import com.example.duoapp.fragmentsCursos.Client.IClientRMI;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import lipermi.handler.CallHandler;
+import lipermi.net.Client;
 
 public class Cursos extends Fragment {
 
     Spinner spinnerIdiomes;
     ListView listIdiomes;
+    List<String> totsElsIdiomes;
+    String[] arrayIdiomes;
 
     public Cursos() {
         // Required empty public constructor
@@ -41,9 +55,39 @@ public class Cursos extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_cursos, container, false);
 
+        totsElsIdiomes = new ArrayList<String>();
+        arrayIdiomes = new String[totsElsIdiomes.size()];
+
+        arrayIdiomes = totsElsIdiomes.toArray(arrayIdiomes);
+
+        listIdiomes = v.findViewById(R.id.cursosList);
+        //ListAdapter adapter = new ListAdapter(this, listIdiomes, List<totsElsIdiomes>);
+
+        //listIdiomes.setAdapter(adapter);
         //spinnerIdiomes = (Spinner) v.findViewById(R.id.spinnerIdiomes);
 
         //spinnerIdiomes.setSelection(0);
         return  v;
+    }
+
+    class client extends AsyncTask<Void, Void, MainActivity> {
+
+        @Override
+        protected MainActivity doInBackground(Void... voids) {
+            try{
+                CallHandler callHandler = new CallHandler();
+
+                int port = 15015;
+                Client client = new Client("ip servidor", port, callHandler);
+                IClientRMI inter = (IClientRMI) client.getGlobal(IClientRMI.class);
+
+                totsElsIdiomes = inter.getTotsIdiomes();
+                client.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
